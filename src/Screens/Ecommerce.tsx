1cd71416527436f9e3fd5d74adaf5b1ca4d4faf3
Ariful Abir <arifulabir10@gmail.com>
2023-08-05 09:41:25 +0600

    git

---
+++ src/Screens/Ecommerce.tsx
@@ -0,0 +1,23 @@
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
+import React from 'react'
+
+const Ecommerce = () => {
+  return (
+        <SafeAreaView>
+            View
+            <View style={styles.mainContainer}>
+            <View style={styles.title}>
+
+                </View>
+
+            </View>
+        </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+    title:{
+
+    }
+})
+export default Ecommerce
