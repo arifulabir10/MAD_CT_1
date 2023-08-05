07119f4ae3d005bc69221b13d1a231d165e8679d
Ariful Abir <arifulabir10@gmail.com>
2023-08-05 10:19:54 +0600

    inside folder

--- src/Screens/Ecommerce.tsx
+++ src/Screens/Ecommerce.tsx
@@ -1,5 +1,6 @@
 import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
 import React from 'react'
+import { Button } from 'react-native';
 
 const Ecommerce = () => {
   return (
@@ -8,8 +9,35 @@ const Ecommerce = () => {
             <View style={styles.mainContainer}>
             <Text style={styles.title}>
                 Ecommerce
-
                 </Text>
+
+                <View style={styles.cartItem}>
+                    <Text style={styles.productName}>Tide Powder</Text>
+                    <Text style={styles.productCategory}>Washing powder</Text>
+                    <Text style={styles.productWeight}>800gm</Text>
+                    <Button title="Add to cart"/>
+                </View>
+
+                <View style={styles.cartItem}>
+                    <Text style={styles.productName}>Onion</Text>
+                    <Text style={styles.productCategory}>Daily Needs</Text>
+                    <Text style={styles.productWeight}>1 kg</Text>
+                    <Button title="Add to cart"/>
+                </View>
+
+                <View style={styles.cartItem}>
+                    <Text style={styles.productName}>Milk Vita</Text>
+                    <Text style={styles.productCategory}>Dairy Product</Text>
+                    <Text style={styles.productWeight}>1 litre</Text>
+                    <Button title="Add to cart"/>
+                </View>
+
+                <View style={styles.cartItem}>
+                    <Text style={styles.productName}>Amul Butter</Text>
+                    <Text style={styles.productCategory}>Dairy Product</Text>
+                    <Text style={styles.productWeight}>500 gm</Text>
+                    <Button title="Add to cart"/>
+                </View>
 
             </View>
         </SafeAreaView>
@@ -18,15 +46,33 @@ const Ecommerce = () => {
 
 const styles = StyleSheet.create({
     mainContainer:{
-        
+        flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
     },
     title:{
-        fontSize: 20,
+        fontSize: 30,
         fontWeight: "bold",
-        
+        margin: 40,
 
+    },
+    cartItem: {
+        height: 100,
+        width: 350,
+        backgroundColor: "skyblue",
+        margin: 8,
+    },
+    productName: {
+        fontSize: 20,
+        fontWeight: "bold"
+    },
+    productCategory: {
+        fontSize: 15,
+        color: "grey"
+    },
+    productWeight: {
+        fontSize: 15,
+        color: "grey"
     }
 })
-export default Ecommerce
+export default Ecommerce;
